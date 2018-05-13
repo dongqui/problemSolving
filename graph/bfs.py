baac87dd2dd641d508b56925c80794bb7ae6c4db
dongjin1025 <kimwinner90@gmail.com>
2018-05-13 19:55:29 +0900

    dfs / bfs

--- graph/bfs.py
+++ graph/bfs.py
@@ -3,7 +3,6 @@ vertex_num, edge_num, start = (int(elem) for elem in input().split())
 
 
 edgeList = []
-queue = [start]
 adjacencyList = [[] for vertex in range(vertex_num + 1)]
 
 for i in range(edge_num):
@@ -12,33 +11,8 @@ for i in range(edge_num):
 for edge in edgeList:
     adjacencyList[edge[0]].append(edge[1])
 
-
-def dfs():
-    stack = [start]
-    visitedList = []
-    blocked = False
-    while stack:
-        current = stack[len(stack) - 1]
-        if current not in visitedList:
-            visitedList.append(current)
-        adjacencyList[current].sort()
-        for neighbor in adjacencyList[current]:
-            if neighbor not in visitedList:
-                stack.append(neighbor)
-                visitedList.append(neighbor)
-                blocked = False
-                break
-            else:
-                blocked = True
-                continue
-        if blocked:
-            stack.pop()
-
-    for i in range(0, len(visitedList)):
-        print(visitedList[i], end=" ")
-
-
 def bfs():
+    queue = [start]
     visitedList = [start]
     while queue:
         current = queue.pop()
@@ -51,5 +25,3 @@ def bfs():
         print(visitedList[i], end=" ")
 
 
-dfs()
-bfs()
