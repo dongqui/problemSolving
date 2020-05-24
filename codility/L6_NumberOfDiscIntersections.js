8d839a15a8b81de708bc768cf2fc663d4bb6d4ed
dongqui <kimwinner90@gmail.com>
2020-05-24 16:09:33 +0900

    Lesson 6 Sorting NumberOfDiscIntersections.js

---
+++ codility/L6_NumberOfDiscIntersections.js
@@ -0,0 +1,25 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function solution(A) {
+  const event = [];
+  for (const [ i, v ] of A.entries()) {
+      event.push([i - v, 1]);
+      event.push([i + v, -1]);
+  }
+  event.sort((a, b) => a[0] - b[0]);
+  
+  let intersection = 0;
+  let intervals = 0;
+
+  for (const e of event) {
+      if (e[1] === -1) {
+          intervals -= 1;
+      } else {
+          intersection += intervals;
+          intervals += 1;
+      }
+  }
+
+  return intersection > 10000000 ? -1 : intersection;
+}
