a906aff4d3b64682963ebe2a9a338d1352a3ce5c
Dongjin <dongqui9@gmail.com>
2022-09-23 12:55:39 +0900

    [level 3] Title: 기둥과 보 설치, Time: 34.91 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/60061. 기둥과 보 설치/기둥과 보 설치.py
@@ -0,0 +1,34 @@
+def impossible(result):
+    COL, ROW = 0, 1
+    for x, y, a in result:
+        if a == 0:
+            if y != 0 and \
+               (x, y - 1, 0) not in result and \
+               (x - 1, y, 1) not in result and \
+               (x, y, 1) not in result:
+                return True
+        else: 
+            if (x, y-1, COL) not in result and \
+               (x+1, y-1, COL) not in result and \
+               not ((x-1, y, ROW) in result and \
+               (x+1, y, ROW) in result):
+                return True
+    
+    return False
+
+def solution(n, build_frame):
+    result = set()
+    
+    for x, y, a, build in build_frame:
+        item = (x, y, a)
+        if build:
+            result.add(item)
+            if impossible(result):
+                result.remove(item)
+        elif item in result:
+            result.remove(item)
+            if impossible(result):
+                result.add(item)
+    answer = map(list, result)
+    
+    return sorted(answer, key = lambda x : (x[0], x[1], x[2]))
